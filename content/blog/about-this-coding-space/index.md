0262e2658eb1e63a51ff765a9f1aa153ab8633c6
RachelChen <iceage91@gmail.com>
2022-07-24 11:55:23 +0800

    Edited portfolio page and articles

--- content/blog/about-this-coding-space/index.md
+++ content/blog/about-this-coding-space/index.md
@@ -3,20 +3,22 @@ title: About this coding space
 createdAt: "2021-05-21T18:50:32.169Z"
 year: "2021"
 month: "05"
-description: This is a web space for displaying portfolio projects showcase and sharing articles about web app coding such as Javascript、Vue、React.
+description: This is a web space for displaying my portfolio projects and sharing articles about web app coding such as Javascript、Vue、React.
 source: file
 tags: General
 slug: /about-this-coding-space/
 embeddedImagesRemote: https://i.imgur.com/dXrC26m.png,https://i.imgur.com/dXrC26m.png,https://i.pinimg.com/564x/b0/49/a7/b049a76d1366c37d6db75a3d47dc3b1e.jpg
 ---
+import {Link} from "gatsby"
 
 ## Introduction
-This is a web space for displaying portfolio projects showcase and sharing articles about web app coding such as Javascript、Vue、React.
+This is a web space for displaying my portfolio projects and sharing articles about web app coding such as Javascript、Vue、React.
 
 ## How to use:
-- The `Portfolio` page introduces my study / working expriences and showcasing personal front end projects.  
-- In the `Blog` page, it logged articles that are related to Javascript、Vue、React.  
-- The `Community` page are some study clubs、technical communities that I have taken part in during my journey of learning web app development. 
+- The <Link to='/portfolio'>Portfolio</Link> page introduces my backgrounds, technical communities that I have been taking part in, and showcasing some personal side projects.  
+- In the <Link to='/blog'>Blog</Link> page, it logged articles that are related to Javascript、Vue、React.  
+- <Link to='/categories'>Categories</Link> page display articles in the order by the date upon creation.
+- <Link to='/rss.xml'>RSS File</Link> is available here so this blog can be followed by using RSS reader.
 
 I built this Gatsby site ([Gatsby's Docs](https://www.gatsbyjs.org)) and use Notion API ([Notion's Docs](https://www.notion.so)) to get articles from Notion database, and deploy by Netlify ([Netlify's Docs](https://www.netlify.com)) so it's easy to set up and maintain to me.
 
@@ -24,4 +26,4 @@ I built this Gatsby site ([Gatsby's Docs](https://www.gatsbyjs.org)) and use Not
 let author = "Rachel_Chen"
 ```
 
-![A computer with shining keyboards and a aside a cup of coffee.](./computer01.jpg)
+![](./computer01.jpg)
--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,8 +1,8 @@
-import * as React from 'react'
-import Layout from '../../components/layout'
-import Seo from '../../components/seo'
-import Bio from '../../components/bio'
-import { graphql } from 'gatsby'
+import * as React from "react"
+import Layout from "../../components/layout"
+import Seo from "../../components/seo"
+import Bio from "../../components/bio"
+import { graphql, Link } from "gatsby"
 
 const Portfolio = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
@@ -12,9 +12,82 @@ const Portfolio = ({ data, location }) => {
       <Layout location={location} title={siteTitle}>
         <Seo />
         <Bio />
-      Coming soon.
-      </ Layout>
-      </div>
+        <h2>About me</h2>
+        <p>
+          Starting 2021, I took part in full-stack web development program which
+          was held by ALPHA Camp, started my journey of JS programming. In
+          addition to build interactive web app by utilizing React, Vue,
+          Gatsby.js, Bootstrap, REST API, GraphQL, etc., I also have practical
+          knowledge and experiences of database works by using MongoDB,
+          Mongoose, Sequelize to MySQL, Firebase.
+        </p>
+        <p>
+          Outside of work, I am a tennis player. I enjoy rock climbing and
+          traveling to discover new landscapes and cultures.
+        </p>
+        <h2>Communities </h2>
+        <p>
+          {" "}
+          Here are the technical communities that I have taken part in during my
+          journey of learning web app development:
+        </p>
+        <ul>
+          <li>
+            <a href="https://discord.com/channels/925294714217967647/945255644271902721">
+              ALPHA Camp
+            </a>
+          </li>
+          <li>
+            <a href="https://discord.com/channels/956136586154737685/956136586611949631">
+              React / JS Study Club
+            </a>
+          </li>
+        </ul>
+        <h2>Personal Side projects</h2>
+        <ul>
+                    <li>
+            <p>
+              Web Blog - a web space for displaying my portfolio projects and sharing articles about web app coding such as Javascript、Vue、React.  This is booted by Gatsby.
+              {" "}
+              <Link to='/blog/gatsby-blog-starter'>Read More</Link>
+            </p>
+            <p>
+              <Link to='/'>
+                Homepage
+              </Link>{" "}
+              /
+              <a href="https://github.com/learnpytest/Rachel-s_Coding_Space">
+                Repository
+              </a>
+            </p>
+          </li>
+          <li>
+            <p>
+              Simple Twitter Clone - This app allows users to create tweets, react to tweets (like/unlike, add
+              comments), get notifications of activities, and follow/unfollow
+              other users. 
+              {" "}
+              <Link to='/blog/simple-twitter-clone'>Read More</Link>
+            </p>
+            <p>
+              <a href="https://learnpytest.github.io/Front_End_Vue_Simple_Twitter/#/login">
+                Homepage
+              </a>{" "}
+              /
+              <a href="https://github.com/learnpytest/Front_End_Vue_Simple_Twitter">
+                Repository
+              </a>
+            </p>
+          </li>
+          <li>
+            <p>vii-message-window</p>
+            <a href="https://www.npmjs.com/package/vii-message-window">
+              NPM
+            </a>
+          </li>
+        </ul>
+      </Layout>
+    </div>
   )
 }
 
